6317252cf3707c6f88e5dfb1e307f558b1a6af39
RinaAmba <k.amba@list.ru>
2025-04-29 21:56:11 +0300

    Add files via upload

---
+++ task 5 Амбарцумян.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+int main() {
+    int size;
+    std::cout << "Введите размер векторов: ";
+    std::cin >> size;
+
+    // Создаем векторы заданного размера
+    vector<double> vec1(size);
+    vector<double> vec2(size);
+
+    // Ввод элементов первого вектора
+    cout << "Введите элементы первого вектора:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> vec1[i];
+    }
+
+    // Ввод элементов второго вектора
+    cout << "Введите элементы второго вектора:\n";
+    for (int i = 0; i < size; ++i) {
+        cin >> vec2[i];
+    }
+
+  
+    double dot_sum = 0.0;
+
+    #pragma omp parallel for reduction(+:dot_sum) num_threads(12)
+    for (int i = 0; i < size; ++i) {
+        dot_sum += vec1[i] * vec2[i];
+    }
+
+    cout << "\nПервый вектор: ";
+    for (double num : vec1) {
+        cout << num << " ";
+    }
+
+    cout << "\nВторой вектор: ";
+    for (double num : vec2) {
+        cout << num << " ";
+    }
+
+    cout << "\nСкалярное произведение: " << dot_sum << endl;
+
+    return 0;
+}
